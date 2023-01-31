ad786e45c27c2ffa55e72e1ef4dff41950a1745c
asma <ga_haidour@esi.dz>
2023-01-31 16:30:34 +0100

    dernier commit

--- my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
+++ my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
@@ -2,6 +2,7 @@ package com.example.myfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
